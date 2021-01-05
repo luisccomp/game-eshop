8370d4dcd6b8b6842d9b600ac59f325ecce13df3
Luís Carlos Sílva Júnior <luisccomp@gmail.com>
2021-01-05 09:37:03 -0300

    Put navbar into core module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,18 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginButtonComponent } from './components/login/login-button.component';
+import { NavbarComponent } from '../core/components/navbar/navbar.component';
+import { LoginButtonComponent } from '../core/components/login/login-button.component';
 import { RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from '../core/components/login/login.component';
 import { FormsModule } from '@angular/forms';
+import { CoreModule } from 'src/core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    LoginButtonComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +23,8 @@ import { FormsModule } from '@angular/forms';
         component: LoginComponent
       }
     ]),
-    FormsModule
+    FormsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/core/components/login/login.component.ts
+++ frontend/src/core/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { UserLogin } from "./UserLogin";
+import { UserLogin } from "../../../app/models/user/UserLogin";
 
 @Component({
   selector: 'app-gamestore-login',
